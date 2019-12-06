c699597cecc8bcef52f87e7ac968156855e95236
Yuriy Rakhletskyy <rakhletskyy22@gmail.com>
2019-12-06 16:26:12 +0200

    Updates

---
+++ precache-manifest.72b7e46f8f8a3e6cc98ac4d5fde08031.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4edd8da67bfec6d9a6d703ebc2b79c6",
+    "url": "/trello-min/index.html"
+  },
+  {
+    "revision": "b9a94d84a2311ecb66c5",
+    "url": "/trello-min/static/css/2.7f669fb7.chunk.css"
+  },
+  {
+    "revision": "bc5f00435e425780ca76",
+    "url": "/trello-min/static/css/main.8e208aa8.chunk.css"
+  },
+  {
+    "revision": "b9a94d84a2311ecb66c5",
+    "url": "/trello-min/static/js/2.e71daf7d.chunk.js"
+  },
+  {
+    "revision": "bc5f00435e425780ca76",
+    "url": "/trello-min/static/js/main.f309d57b.chunk.js"
+  },
+  {
+    "revision": "c5df7349172b6c9704bf",
+    "url": "/trello-min/static/js/runtime-main.ba4b1bc2.js"
+  },
+  {
+    "revision": "5ab54a2d9cb33738c8493febec5f9afd",
+    "url": "/trello-min/static/media/bg.5ab54a2d.jpg"
+  }
+]);
